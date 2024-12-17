2e7e8036d6130be5f669ec4af30d0246b4e20d1e
SPRansing <shubhamransing24@gmail.com>
2024-12-17 12:58:32 +0530

    New things added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    int empid =111;
+    int empid = 111;
+    static int departID = 400;
+
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             System.out.println("This is new feature feature 1 added");
             System.out.println("This is new feature feature 2");
+            System.out.println("Emp id is=" + new Main().empid);
         }
     }
 }
