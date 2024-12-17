4ae9cdfc8aa3a79d39776d80f96c6999523353be
SPRansing <shubhamransing24@gmail.com>
2024-12-17 11:22:51 +0530

    Update Main.java
    emp id changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    int empid =105;
+    int empid =109;
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             System.out.println("This is new feature feature 1 added");
+            System.out.println("This is new feature feature 2");
         }
     }
 }
