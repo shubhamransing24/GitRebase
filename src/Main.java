5cf4fd45b17e9d2b11609f3368b5460038f3059c
shubhamransing24 <shubhamransing24@gmail.com>
2025-01-15 14:46:37 +0530

    f1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("m1");
-        System.out.println("m2");
-        System.out.println("m3");
         System.out.println("f1");
-        System.out.println("f2");
-        System.out.println("mm1");
-        System.out.println("mm2");
-        System.out.println("ff1");
-        System.out.println("ff2");
-        System.out.println("mmm1");
-        System.out.println("fff1");
-        System.out.println("fff2");
-        System.out.println("fff3");
-        System.out.println("fff4");
-        System.out.println("fff5");
-        System.out.println("mmm2");
-        System.out.println("fff3");
-        System.out.println("fff4");
-         System.out.println("fff1");
     }
 }
